2f5dc7d9edcb93465e80c18d099edf73223cfa74
Reinaldo Cuevas <rantony52@gmail.com>
2024-12-22 12:06:15 -0600

    re making body app and adding new style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
+//notes: validate the list is not empty
+//allow the user to write down a new task and add it.
+//erase button for each task
+//checkbox, change the font color of a completed task
+
 const menu = document.getElementById('menu');
 const elements = document.querySelectorAll('.elements');
 const nav =  document.getElementById('navbar');
-console.log(elements);
+const adding = document.getElementById('adding');
+const cuerpo = document.getElementById('body');
+const borrar = document.getElementById('erase');
+let notas = document.querySelectorAll('content-body-app');
+
 menu.addEventListener('click', () =>{
     elements.forEach( elemento =>{
         elemento.classList.toggle('elements-hidden');
     })
     nav.classList.toggle('menu-hidden');
 });
+adding.addEventListener('click', () =>{
+    const nuevoDiv = document.createElement('div');
+    nuevoDiv.classList = 'content-body-app';
+    nuevoDiv.innerHTML = `
+                    <div class="adding-container">+</div>
+                <div class="activity">
+                    <p></p>
+                    <div class="time">00:00</div>
+                    <div id="erase">-</div>
+                </div>
+    `;
+    cuerpo.appendChild(nuevoDiv);
+
+    notas = document.querySelectorAll('.content-body-app');
+    console.log(notas);
+});
+borrar.addEventListener('click', () =>{
+    
+})
